3886ecedf45603faedea2fa664f98d039602167d
William Fan <willshaofan@gmail.com>
2016-12-29 14:33:20 -0600

    use stupid docker env variable syntax

--- app/jobs/subscribe_user_to_mailing_list_job.rb
+++ app/jobs/subscribe_user_to_mailing_list_job.rb
@@ -3,6 +3,6 @@ class SubscribeUserToMailingListJob < ApplicationJob
 
   def perform(user)
     gb = Gibbon::Request.new
-    gb.lists(ENV["MAILCHIMP_LIST_ID"]).members.create(body: {email_address: user.email, status: "subscribed", merge_fields: {USERNAME: user.username}})
+    gb.lists(ENV['MAILCHIMP_LIST_ID']).members.create(body: {email_address: user.email, status: "subscribed", merge_fields: {USERNAME: user.username}})
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -85,7 +85,8 @@ class User < ApplicationRecord
   private
 
   def subscribe_user_to_mailing_list
-    SubscribeUserToMailingListJob.perform_later(self)
+    gb = Gibbon::Request.new
+    gb.lists(ENV["MAILCHIMP_LIST_ID"]).members.create(body: {email_address: self.email, status: "subscribed", merge_fields: {USERNAME: self.username}})
   end
 
   protected
