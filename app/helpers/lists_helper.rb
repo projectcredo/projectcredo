f078170147819fc99aaac15d82037477a0294911
Brian Kung <brian@callmekung.com>
2016-12-29 19:22:42 -0600

    Disables search bar if not signed in

--- app/helpers/lists_helper.rb
+++ app/helpers/lists_helper.rb
@@ -4,7 +4,7 @@ module ListsHelper
   end
 
   def current_user_can_edit? list
-    current_user && current_user.can_edit?(list)
+    current_user && (current_user.can_edit?(list) || list.accepts_public_contributions?)
   end
 
   def current_user_can_view? list
