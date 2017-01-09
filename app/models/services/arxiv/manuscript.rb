4fb772655dd8496d045d442bf074e69147b2c40c
alexkcode <alexiskwan.uiuc@gmail.com>
2017-01-08 17:00:02 -0700

    refactored tests to be more specific. tried to overload gem Arxiv.gem method to get desired raw api response

---
+++ app/models/services/arxiv/manuscript.rb
@@ -0,0 +1,8 @@
+module Arxiv
+  class Manuscript
+
+    # element :raw_response, Nokogiri::XML::NodeSet
+    attr_accessor :raw_response
+
+  end
+end
--- test/models/arxiv_test.rb
+++ test/models/arxiv_test.rb
@@ -2,16 +2,26 @@ require 'test_helper'
 
 class ArxivTest < ActiveSupport::TestCase
   # testing Arxiv gem integration
+
+  test "arxiv api response no title handling 0000.000 edge case" do
+    assert_raises(Arxiv::Error::ManuscriptNotFound) {
+      manuscript = Arxiv.get('0000.0000', enable_raw: false)
+    }
+  end
   
   test "arxiv id not found error handling" do
     # no errors should be thrown
-    test_resource = Arxiv::Resource.new '0000.0000'
+    assert_nothing_raised {
+      test_resource = Arxiv::Resource.new '0000.0000'
+    }
     assert !test_resource.response
   end
 
   test "arxiv id malformed error handling" do
     # no errors should be thrown
-    test_resource = Arxiv::Resource.new '1202.08190'
+    assert_nothing_raised {
+      test_resource = Arxiv::Resource.new '1202.08190'
+    }
     assert !test_resource.response
   end
 
