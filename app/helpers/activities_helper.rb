d964aace91d5ac345f7092d0a26d0777d23387f1
William Fan <willshaofan@gmail.com>
2017-03-21 17:10:02 -0500

    create comment notifications

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -2,7 +2,7 @@ module ActivitiesHelper
   def create_activity(actable: ,activity_type: ,addable: nil)
     Activity.create(user_id: current_user.id,
                         activity_type: activity_type,
-                        addable_id: addable.id,
+                        addable_id: if addable.nil? then nil else addable.id end,
                         actable_type: actable.class.name,
                         actable_id: actable.id)
   end
