228d4550218fcd7f7337d15d761bb9ff7bfd156f
William Fan <willshaofan@gmail.com>
2017-04-25 01:27:30 -0500

    add comments to activity migration

--- db/migrate/20170424232813_modify_activities_with_lists_and_references.rb
+++ db/migrate/20170424232813_modify_activities_with_lists_and_references.rb
@@ -1,6 +1,6 @@
 class ModifyActivitiesWithListsAndReferences < ActiveRecord::Migration[5.0]
   def up
-    Activity.where("activity_type = 'created' or activity_type = 'added'").each(&:destroy)
+    Activity.delete_all
 
     List.find_each(batch_size: 50) do |l|
       Activity.create(user_id: l.owner.id, activity_type: "created", actable: l, created_at: l.created_at, updated_at: l.created_at) unless Activity.find_by(actable: l, activity_type: "created")
@@ -8,10 +8,14 @@ class ModifyActivitiesWithListsAndReferences < ActiveRecord::Migration[5.0]
       l.references.each do |r|
         Activity.create(user_id: r.user_id, activity_type: "added", actable: l, addable: r, created_at: r.created_at, updated_at: r.created_at) unless Activity.find_by(actable: l, addable: r, activity_type: "added")
       end
+
+      l.comments.each do |c|
+        Activity.create(user_id: c.user_id, activity_type: "commented", actable: l, addable: c, created_at: c.created_at, updated_at: c.created_at) unless Activity.find_by(actable: l, addable: c, activity_type: "commented")
+      end
     end
   end
 
   def down
-    Activity.where("activity_type = 'created' or activity_type = 'added'").each(&:destroy)
+    Activity.delete_all
   end
 end
