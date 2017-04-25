209e70e020dce656f26b03b4c294b6bb1c3574d8
William Fan <willshaofan@gmail.com>
2017-04-24 23:45:44 -0500

    simplify create activity

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -2,10 +2,8 @@ module ActivitiesHelper
   def create_activity(actable: ,activity_type: ,addable: nil)
     Activity.create(user_id: current_user.id,
                         activity_type: activity_type,
-                        addable_id: if addable.nil? then nil else addable.id end,
-                        addable_type: if addable.nil? then nil else addable.class.name end,
-                        actable_type: actable.class.name,
-                        actable_id: actable.id)
+                        addable: addable,
+                        actable: actable)
   end
 
   def create_activity_and_notifications(actable: ,activity_type:, users: ,addable: nil)
