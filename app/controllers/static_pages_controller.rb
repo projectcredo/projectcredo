b72eaf90ebf2b8fc11efddf3cb300922c7c0aa46
William Fan <willshaofan@gmail.com>
2017-05-28 18:14:57 -0500

    SSL renew

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -10,12 +10,8 @@ class StaticPagesController < ApplicationController
   end
 
   def lets_encrypt
-    if secure_compare params[:id], ENV['8sNcrPS1EVyT3z54lUNVLWxe9y5PdbpasbXXjgd48y8.z3DKE5GPOwED-2OCcZfwwCsOEKy7duPdBUarIV-Hvss
-']
-      render text: ENV['8sNcrPS1EVyT3z54lUNVLWxe9y5PdbpasbXXjgd48y8.z3DKE5GPOwED-2OCcZfwwCsOEKy7duPdBUarIV-Hvss
-']
-    else
-      raise ActionController::RoutingError.new('Not Found')
-    end
+
+      render text: ENV['JpdrWI4yOh6Zb1CqEnUr-ZPDaKGpXHEm2T_c-Uvr75g.z3DKE5GPOwED-2OCcZfwwCsOEKy7duPdBUarIV-Hvss']
+
   end
 end
