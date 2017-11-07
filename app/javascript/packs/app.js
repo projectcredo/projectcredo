a881a3c645c7ffe55c8fa7c0978852d77a918352
wsf246 <willshaofan@gmail.com>
2017-11-07 00:35:31 -0600

    Revert "Develop"

--- app/javascript/packs/app.js
+++ app/javascript/packs/app.js
@@ -5,8 +5,6 @@
 // like app/views/layouts/application.html.erb.
 // All it does is render <div>Hello Vue</div> at the bottom of the page.
 
-import '../legacy/general'
-
 import Vue from 'vue'
 import Hello from '../components/hello.vue'
 
