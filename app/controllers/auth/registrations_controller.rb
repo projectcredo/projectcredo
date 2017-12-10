f755a6fe745f0c2715a5b6230e9c0d2facea65c9
Vedmant <vedmant@gmail.com>
2017-12-10 11:09:46 +0700

    Fixed an issue with removing attachment

--- app/controllers/auth/registrations_controller.rb
+++ app/controllers/auth/registrations_controller.rb
@@ -2,10 +2,13 @@ class Auth::RegistrationsController < Devise::RegistrationsController
   prepend_before_action :authenticate_scope!, only: [:edit, :update, :destroy, :remove_attachment]
 
   def remove_attachment
-    redirect_to request.referer, error: 'Invalid attachment type' unless resource.has_attribute? params[:type]
+    unless resource.has_attribute?(params[:type]) and resource.public_send(params[:type]).respond_to?(:destroy)
+      redirect_to request.referer || root_path, error: 'Invalid attachment type'
+      return
+    end
 
     resource.public_send(params[:type]).destroy
     resource.save
-    redirect_back notice: 'Attachment was deleted'
+    redirect_back notice: 'Attachment was deleted', fallback_location: root_path
   end
 end
