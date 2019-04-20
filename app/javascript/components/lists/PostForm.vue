4120aaff338c575dd62a0c21f0c970a454a9c0cd
Vedmant <vedmant@gmail.com>
2019-04-20 11:28:27 +0700

    URL preview

--- app/javascript/components/lists/PostForm.vue
+++ app/javascript/components/lists/PostForm.vue
@@ -1,49 +1,51 @@
 <template>
   <div class="list-post-form">
-    <textarea v-model="content" class="lpf-textarea" placeholder="What are you reading?"></textarea>
-    <div>{{ urls }}</div>
-    <button class="btn btn-default" :disabled="loadingUrlInfo">Post</button>
+    <textarea v-model="content" ref="textarea" class="lpf-textarea" placeholder="What are you reading?"></textarea>
+    <url-preview v-if="url" :info="urlInfo" :loading="loadingUrlInfo"></url-preview>
+    <button class="btn btn-primary" :disabled="loadingUrlInfo">Post</button>
+    <a v-if="url" class="lpf-clear" href="#" @click.prevent="url = ''">clear</a>
   </div>
 </template>
 
 <script>
 import urlRegex from 'url-regex'
 import debounce from 'lodash-es/debounce'
+import isEmpty from 'lodash-es/isEmpty'
 import axios from '../../services/axios';
+import UrlPreview from './UrlPreview'
+import autosize from 'autosize'
 
 export default {
   props: ['list'],
 
+  components: {
+    UrlPreview,
+  },
+
   data () {
     return {
       content: '',
-      urls: [],
       url: '',
-      openGraph: {},
+      urlInfo: {},
       loadingUrlInfo: false,
     }
   },
 
   watch: {
     content (val) {
-      this.findUrls()
-    },
-
-    urls (val) {
-      if (val.length && this.url !== val[0]) this.url = val[0]
-      else if (! val.length) this.url = ''
+      if (! this.url) this.findUrl()
     },
 
     url (val) {
       if (val) this.loadUrlInfo()
+      else this.urlInfo = {}
     },
   },
 
   methods: {
-    findUrls: debounce(function () {
-      this.urls = []
+    findUrl: debounce(function () {
       this.content.replace(urlRegex(), url => {
-        this.urls.push(url)
+        if (! this.url) this.url = url
       })
     }, 300),
 
@@ -51,12 +53,16 @@ export default {
       this.loadingUrlInfo = true
       axios.post('/posts/load-open-graph', {url: this.url})
         .then(res => {
-          console.log(res.data)
+          this.urlInfo = res.data
         })
         .then(() => {
           this.loadingUrlInfo = false
         })
     },
+  },
+
+  mounted () {
+    this.$refs.textarea.addEventListener('focus', () => autosize(this.$refs.textarea))
   },
 }
 </script>
---
+++ app/javascript/components/lists/UrlPreview.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="url-preview">
+    <div v-if="loading">Fetching preview...</div>
+    <div v-else>
+      <div class="urlp-image cropped-image cropped-image-3by1" v-if="info.images.length"><div><img :src="info.images[0].src" alt=""></div></div>
+      <div class="urlp-title">{{ info.title }}</div>
+      <div class="urlp-descr">{{ info.description }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['info', 'loading'],
+}
+</script>
