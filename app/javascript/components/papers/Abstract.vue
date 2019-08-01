844c1f878e8acdb2a965ce4f617523e3329d3a8e
Vedmant <vedmant@gmail.com>
2019-08-01 18:09:44 +0300

    Added newlines to papers abstract

--- app/javascript/components/papers/Abstract.vue
+++ app/javascript/components/papers/Abstract.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <span class="newlines">{{ abstract | truncate(250, truncateAbstract) }}</span>
+    <span class="newlines" v-html="text"></span>
     <a class="action-link" v-if="abstract.length > 250" @click.stop.prevent="truncateAbstract = ! truncateAbstract">
       {{ truncateAbstract ? 'see more' : 'see less' }}
     </a>
@@ -8,6 +8,7 @@
 </template>
 
 <script>
+  import Vue from 'vue'
   export default {
     props: ['abstract'],
 
@@ -15,6 +16,12 @@
       return {
         truncateAbstract: true
       }
+    },
+
+    computed: {
+      text () {
+        return Vue.options.filters.truncate(this.abstract, 250, this.truncateAbstract).replace(/(?:\r\n|\r|\n)/g, '<br>')
+      },
     },
   }
 </script>
