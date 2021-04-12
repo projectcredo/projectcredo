0e0288a06fa16381c76ae11cbe2c36e51cb703b9
Vedmant <vedmant@gmail.com>
2021-04-12 16:18:03 +0300

    Show smaller number of activities

--- app/controllers/api/activities_controller.rb
+++ app/controllers/api/activities_controller.rb
@@ -1,6 +1,6 @@
 class Api::ActivitiesController < Api::ApplicationController
   def index
-    @visible_lists = List.by_activity.first(30)
+    @visible_lists = List.by_activity.first(10)
     @include_activities = true
     render 'jbuilders/_lists.json.jbuilder', {locals: {lists: @visible_lists}}
   end
